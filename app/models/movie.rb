81de6ee8d92680ae95eb1d1df33cbcc5b9961eb4
m.rateb <rateb@aucegypt.edu>
2024-02-15 17:57:23 +0200

    average rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,9 @@ class Movie < ApplicationRecord
   has_many :actors, through: :jobs
   has_many :movie_locations
   has_many :locations, through: :movie_locations
+  has_many :reviews
+
+  def average_rating
+    reviews.average(:stars)
+  end
 end
